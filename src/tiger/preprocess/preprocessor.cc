5291eb61bf12e4aff0fec60bac472501da9a4463
Szy0127 <1799853523@qq.com>
2023-06-11 22:50:16 +0800

    feat: can also use 4 space to indent, fix preprocess

--- src/tiger/preprocess/preprocessor.cc
+++ src/tiger/preprocess/preprocessor.cc
@@ -3,8 +3,38 @@
 #include <iostream>
 #include <stack>
 #include <tuple>
+
 Preprocessor::Preprocessor(std::string ifname,std::string ofname):ifname(ifname),ofname(ofname){}
 
+std::tuple<int,int> get_indent(std::string line){
+    int cur_indent = 0;
+    int index = 0;
+    auto len = line.length();
+    while(index < len && line[index++]=='\t'){
+        cur_indent++;
+    }
+    index--;
+    int space_count = 0;
+    while(index < len && line[index++]==' '){
+        space_count++;
+        if(space_count == 4){
+            space_count = 0;
+            cur_indent++;
+        }
+    }
+    index--;
+    //error if not multiple of 4
+    return {cur_indent,index};
+}
+
+bool empty_line(std::string line){
+    for(auto c:line){
+        if(c!=' ' && c!='\t' && c != '\n'){
+            return false;
+        }
+    }
+    return true;
+}
 
 void Preprocessor::preprocess(){
     std::ifstream input(ifname);
@@ -17,15 +47,12 @@ void Preprocessor::preprocess(){
     bool first = true;
     std::stack<int> indentation;
     while(!input.eof() || !buf.empty()){
-        if(buf.empty()){
-            std::getline(input,buf);
-            continue;
+        bool empty = empty_line(buf);
+        auto [cur_indent,index] = get_indent(buf);
+        if(empty){
+            cur_indent = 0;
         }
-        int cur_indent = 0;
-        while(buf[cur_indent]=='\t'){
-            cur_indent++;
-        }
-        // std::cout<<"buf:#"<<buf<<"#"<<cur_indent<<std::endl;
+        // std::cout<<"buf:"<<buf.length()<<"#"<<int(buf[0])<<buf<<"#"<<cur_indent<<std::endl;
         if(indentation.empty() ||  cur_indent>indentation.top()){
             indentation.push(cur_indent);
             output<<"("<<std::endl;
@@ -37,10 +64,13 @@ void Preprocessor::preprocess(){
                 output<<")"<<std::endl;
             }
         }
-        if(!first){
-            output<<";"<<std::endl;
+        if(!empty && buf[index] != '#'){
+            if(!first){
+                output<<";"<<std::endl;
+            }
+            output<<buf;
         }
-        output<<buf;
+
         if(input.eof()){
             break;
         }
